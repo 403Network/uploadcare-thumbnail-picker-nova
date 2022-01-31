486b6cac4488d4cddb2e74a3f55d6b58ddfba1ca
Marcus Coleman <Sunhat@users.noreply.github.com>
2022-01-31 01:00:07 +0000

    Fix naming

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,5 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-uploadcare-thumbnail-picker-nova', require('./components/IndexField'))
-  Vue.component('detail-uploadcare-thumbnail-picker-nova', require('./components/DetailField'))
-  Vue.component('form-uploadcare-thumbnail-picker-nova', require('./components/FormField'))
+  Vue.component('index-uploadcare-thumbnail-picker', require('./components/IndexField'))
+  Vue.component('detail-uploadcare-thumbnail-picker', require('./components/DetailField'))
+  Vue.component('form-uploadcare-thumbnail-picker', require('./components/FormField'))
 })
